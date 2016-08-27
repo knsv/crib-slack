f211aa5d9a16751de90670e42fd096b32a931511
Knut Sveidqvist <knut@sveido.com>
2016-08-27 17:33:37 +0200

    First version

---
+++ src/index.js
@@ -0,0 +1,87 @@
+/**
+ * Created by knut on 2016-04-17.
+ */
+var cribMq = require('../../crib-mq');
+var cribLog = require('../../crib-log/src/api');
+var log = cribLog.createLogger('crib-slack','debug');
+
+log.info('Starting Slack service using this buss: ', process.env.CRIB_BUSS_URL);
+var buss = cribMq.register('crib-slack');
+log.debug('Starting Slack service at this path: ', process.cwd());
+var db = {};
+
+var SlackBot = require('slackbots');
+
+// create a bot
+var bot = new SlackBot({
+    token: process.env.CRIB_SLACK_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
+    name: 'Crib'
+});
+
+bot.on('start', function() {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':alien:'
+    };
+
+    bot.postMessageToChannel('general', 'ជំរាបសួរមិត្តភក្តិ, I am am ready to serve!', params);
+
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
+    //bot.postMessageToChannel('general', 'Grrrr!', params);
+
+    // // define existing username instead of 'user_name'
+    // bot.postMessageToUser('user_name', 'meow!', params);
+    //
+    // // If you add a 'slackbot' property,
+    // // you will post to another user's slackbot channel instead of a direct message
+    // bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' });
+    //
+    // // define private group instead of 'private_group', where bot exist
+    // bot.postMessageToGroup('private_group', 'meow!', params);
+});
+
+bot.on('message', function(msg) {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':dog:'
+    };
+
+
+
+    if(msg.type !== 'message'){
+        return;
+    }
+
+    log.debug(msg);
+
+    let message;
+    if(msg.attachments && msg.username === 'IFTTT'){
+        message = msg.attachments[0];
+    }else{
+        message = msg;
+    }
+
+
+    if(message.text && message.text.match(/KNUT_LEAVES_HOME/)){
+        bot.postMessageToChannel('general', 'leaves hooome ouou!', params);
+        log.debug('KNUT_LEAVES_HOME');
+        buss.emit('KNUT_LEAVES_HOME', [0]);
+    }
+    if(message.text && message.text.match(/KNUT_LEAVES_WORK/)){
+        bot.postMessageToChannel('general', 'leaves work, ouahoo!', params);
+        log.debug('KNUT_LEAVES_WORK');
+        buss.emit('KNUT_LEAVES_WORK', [0]);
+    }
+    if(message.text && message.text.match(/KNUT_COMES_HOME/)){
+        bot.postMessageToChannel('general', 'comes home, woff woff!', params);
+        log.debug('KNUT_COMES_HOME');
+        buss.emit('KNUT_COMES_HOME', [0]);
+    }
+    if(message.text && message.text.match(/KNUTS_DAILY_STEPS/)){
+        bot.postMessageToChannel('general', 'Woff woff, husse duktig!', params);
+        log.debug('KNUTS_DAILY_STEPS');
+        buss.emit('KNUT_COMES_HOME', [0]);
+    }
+});
+
+log.info('Slack service STARTED', process.cwd());
